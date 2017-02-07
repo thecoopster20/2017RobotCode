a3151a7cee0fab1a5f5356e72a4be79f65d84bc7
thecoopster20 <thecoopster20@gmail.com>
2017-02-07 18:43:01 -0500

    Changes From 2/7/17

--- 2017RobotCode/src/org/usfirst/frc/team3602/robot/Robot.java
+++ 2017RobotCode/src/org/usfirst/frc/team3602/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.vision.VisionThread;
@@ -115,6 +116,7 @@ public class Robot extends IterativeRobot {
     				
     				//outputs the desired image to the switcher
     				outputStream.putFrame(image);
+    				
     			}
             
         	});
