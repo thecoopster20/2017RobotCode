dae4b76640c96e205669f47e7fbca4016b12fc8c
thecoopster20 <thecoopster20@gmail.com>
2017-02-09 19:31:13 -0500

    Updated Vision Build
    Increased target width constraints to possibly allow for the targeting
    of both the boiler and the gear tape

--- 2017RobotCode/vision/src/main/java/GripPipeline.java
+++ 2017RobotCode/vision/src/main/java/GripPipeline.java
@@ -1,9 +1,4 @@
-
 import java.io.File;
-
-
-
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.HashMap;
-
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 import org.opencv.core.*;
 import org.opencv.core.Core.*;
@@ -39,43 +32,42 @@ public class GripPipeline {
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
-	
-	
 
 	/**
 	 * This is the primary method that runs the entire pipeline and updates the outputs.
 	 */
-		public void process(Mat source0) {
+	public void process(Mat source0) {
 		// Step RGB_Threshold0:
 		Mat rgbThresholdInput = source0;
-		double[] rgbThresholdRed = {213.70056497175142, 255.0};
-		double[] rgbThresholdGreen = {216.10169491525423, 255.0};
-		double[] rgbThresholdBlue = {213.7005649717514, 255.0};
+		double[] rgbThresholdRed = {208.89830508474577, 255.0};
+		double[] rgbThresholdGreen = {199.29378531073445, 255.0};
+		double[] rgbThresholdBlue = {187.28813559322032, 255.0};
 		rgbThreshold(rgbThresholdInput, rgbThresholdRed, rgbThresholdGreen, rgbThresholdBlue, rgbThresholdOutput);
 
 		// Step Find_Contours0:
 		Mat findContoursInput = rgbThresholdOutput;
-		boolean findContoursExternalOnly = true;
+		boolean findContoursExternalOnly = false;
 		findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
 
 		// Step Filter_Contours0:
 		ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
-		double filterContoursMinArea = 50.0;
-		double filterContoursMinPerimeter = 30.0;
-		double filterContoursMinWidth = 0;
-		double filterContoursMaxWidth = 30.0;
-		double filterContoursMinHeight = 0;
-		double filterContoursMaxHeight = 50.0;
-		double[] filterContoursSolidity = {86.64103200198461, 100.0};
-		double filterContoursMaxVertices = 1000000;
-		double filterContoursMinVertices = 0;
+		double filterContoursMinArea = 45.0;
+		double filterContoursMinPerimeter = 0.0;
+		double filterContoursMinWidth = 10.0;
+		double filterContoursMaxWidth = 320.0;
+		double filterContoursMinHeight = 0.0;
+		double filterContoursMaxHeight = 100.0;
+		double[] filterContoursSolidity = {94.80800114899006, 100.0};
+		double filterContoursMaxVertices = 1000000.0;
+		double filterContoursMinVertices = 0.0;
 		double filterContoursMinRatio = 0.0;
-		double filterContoursMaxRatio = 1.0;
+		double filterContoursMaxRatio = 10.0;
 		filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
 
 		// Step Convex_Hulls0:
-		ArrayList<MatOfPoint> convexHullsContours = filterContoursOutput;
-		convexHulls(convexHullsContours, convexHullsOutput);
+		ArrayList<MatOfPoint> convexHulls0Contours = filterContoursOutput;
+		convexHulls(convexHulls0Contours, convexHullsOutput);
+
 	}
 
 	/**
@@ -109,7 +101,6 @@ public class GripPipeline {
 	public ArrayList<MatOfPoint> convexHullsOutput() {
 		return convexHullsOutput;
 	}
-
 
 	/**
 	 * Segment an image based on color ranges.
